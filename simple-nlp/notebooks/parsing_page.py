2a257e2d9dd6a53ef06e0a22b12b6bac9350be29
Aleksey Bondarenko <bondaleksey@gmail.com>
2022-04-26 10:52:50 +0300

    add function parsing_author_page to parsing_page file

--- simple-nlp/notebooks/parsing_page.py
+++ simple-nlp/notebooks/parsing_page.py
@@ -1,9 +1,41 @@
 import re
+
 def parsing_author_page(soup):
-    pass
+    papers={}
+    pubs= soup.find_all('td', attrs={'align':'left', 'valign':"top"})
+    for indx, pub in enumerate(pubs[1:]):         
+        papers[indx+1] = get_paper_info(pub)  
+    return papers
+
+
+
+def get_paper_info(pub):
+    result = {}
+    name = re.sub('[\n\t\xa0]','',pub.text)
+    name = re.sub(' +',' ',name)
+    name = name.rstrip(' ')
+    name = name.lstrip(' ')    
+    result['name'] = name      
+    for a in pub.find_all('a'):               
+        link = a.get('href')
+        if 'elibrary.ru' in link:
+            # result['elib_link'] = link
+            result['elib_id'] = link.replace("https://elibrary.ru/item.asp?id=","")
+            # re.findall('\d+',link)[0]
+        if 'mathnet.ru' in link:
+            result['mn_link'] = link.replace("http://mi.mathnet.ru/","")
+    return result
+
 
 def parsing_article_page(soup):
-    
+    # print("Однажды, в студеную зимнюю пору\n\
+    #     Я из лесу вышел; был сильный мороз.\n\
+    #         Гляжу, поднимается медленно в гору\n\
+    #             Лошадка, везущая хворосту воз.\n\
+    #                 И шествуя важно, в спокойствии чинном,\n\
+    #                     Лошадку ведет под уздцы мужичок\n\
+    #                         В больших сапогах, в полушубке овчинном,\n\
+    #                             В больших рукавицах… а сам с ноготок! ")        
     doi = None
     for item in soup.find_all("a",  attrs={"class":"SLink"}):
         href = item.get("href")
@@ -13,13 +45,27 @@ def parsing_article_page(soup):
     for line in soup.find_all('td',  attrs={'valign':"top"}):    
         if "Аннотация:" in line.text or "Abstract:" in line.text:
             mystr = line.text    
-            base_info = line.i.text 
+            # reference = line.i.text 
               
     abstract = get_next_paragraph(mystr, "Аннотация:")
-    kwords = get_next_paragraph(mystr, "Ключевые"+"\xa0"+"слова:")
-    udk = get_next_paragraph(mystr, "УДК:")
-    doi = get_next_paragraph(mystr, "DOI:")
-    bib = get_next_paragraph(mystr,"Образец"+"\xa0"+"цитирования:")
+    if abstract is None:
+        abstract = get_next_paragraph(mystr, "Abstract:")
+    keywords = get_next_paragraph(mystr, "Ключевые"+"\xa0"+"слова:")    
+    if keywords is None:
+        keywords = get_next_paragraph(mystr, "Keywords:")
+    if doi is None:
+        doi = get_next_paragraph(mystr, "DOI:").replace("https://doi.org/","")
+    udk = get_next_paragraph(mystr, "УДК:")                
+    reference = get_next_paragraph(mystr,"Образец"+"\xa0"+"цитирования:")
+    if reference is None:
+        reference = get_next_paragraph(mystr,"Образец цитирования:")
+    res = {"Abstract":abstract,
+            "Keywords":keywords,
+            "DOI":doi,
+            "UDK":udk,
+            "Reference":reference}
+    # print(res)
+    return res
     
 def get_next_paragraph(text, phrase):
     result = None
